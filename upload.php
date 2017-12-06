6c376b093633c8c4f4b5fe7c9e137aede3582553
BlackScorpion <prbr@ymail.com>
2017-12-05 23:24:09 -0200

    Funcional como WEB

--- upload.php
+++ upload.php
@@ -7,12 +7,12 @@ if(isset($_POST['script']) && isset($_POST['action'])){
 	if($_POST['action'] == 'save'){
 		file_put_contents(__DIR__.'/libs/page.scpt',$_POST['script']);
 		
-		exit(file_get_contents(__DIR__.'/libs/page.scpt'));
+		exit(file_get_contents(__DIR__.'/libs/page.scpt')); 
 		exit(json_encode(['error'=>false]));
 	}
 	if($_POST['action'] == 'load'){
 		exit(json_encode(['error'=>false,
-						  'script'=>file_get_contents(__DIR__.'/libs/page.scpt'),
+						  'script'=>file_get_contents(__DIR__.'/libs/page_example.scpt'),
 						  'name'=>'page.sctp',
 						  'action'=>'load']));
 	}
